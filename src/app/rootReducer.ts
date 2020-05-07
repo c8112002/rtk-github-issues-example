d179f8c99285e69ea52f976c3d2e22ab4745f2a7
sato <sato_m@mediaxis.jp>
2020-05-07 19:03:33 +0900

    Add a slice for tracking comments data

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit'
 import issuesDisplayReducer from '../features/issueDisplay/issuesDisplaySlice'
 import repoDetailReducer from '../features/repoSearch/repoDetailsSlice'
 import issuesReducer from '../features/issuesList/issuesSlice'
+import commentsReducer from '../features/issueDetails/commentsSlice'
 
 const rootReducer = combineReducers({
   issuesDisplay: issuesDisplayReducer,
   repoDetails: repoDetailReducer,
   issues: issuesReducer,
+  comments: commentsReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
